b5d9f611b309922bc3e14e9a1ab20c53f547709e
vikramsamak <vikramsamak02@gmail.com>
2025-11-08 23:35:49 +0530

    feat : protect dashboard layout.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { useAuth } from "@/hooks";
+import { redirect } from "next/navigation";
+
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { session } = useAuth();
+
+  if (!session) {
+    redirect("/auth/signin");
+  }
+
   return <section>{children}</section>;
 }
