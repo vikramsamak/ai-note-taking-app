37f9b53c6aaf4943d5a0b29884c2f4817c34c66b
vikramsamak <vikramsamak02@gmail.com>
2025-11-09 12:20:07 +0530

    feat : add resuable api functions into lib directory for notes.

---
+++ lib/api/notes.ts
@@ -0,0 +1,45 @@
+import { notesService } from "@/services";
+import { Note } from "@/types";
+
+export async function fetchNotes() {
+  try {
+    return await notesService.get<Note[]>();
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
+}
+
+export async function createNote(data: { title: string; content: string }) {
+  try {
+    return await notesService.post<Note>({ data });
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
+}
+
+export async function updateNote(
+  id: string,
+  data: { title?: string; content?: string; ummary?: string; tags?: string[] }
+) {
+  try {
+    return await notesService.put<Note>({ id, data });
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
+}
+
+export async function deleteNote(id: string) {
+  try {
+    return await notesService.delete<{ success: boolean }>({ id });
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
+}
