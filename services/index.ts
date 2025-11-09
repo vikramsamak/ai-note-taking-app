09917cabac1e774246670c8b322fe2e861cd98a3
vikramsamak <vikramsamak02@gmail.com>
2025-11-09 12:20:53 +0530

    refactor : remove unwanted service and fix service name.

--- services/index.ts
+++ services/index.ts
@@ -1,8 +1,6 @@
 import { ApiService } from "./apiService";
 
 //Service instances for different API endpoints
-export const notesServicePath = new ApiService("/notes");
+export const notesService = new ApiService("/notes");
 
-export const tagsServicePath = new ApiService("/tags");
-
-export const usersServicePath = new ApiService("/users");
+export const usersService = new ApiService("/users");
